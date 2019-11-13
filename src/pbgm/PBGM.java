0469960a7dadd312bd1e42ce6b24ffa251c58565
Igor Lemes <igor.lemes@live.com>
2019-11-12 23:32:35 -0300

    Resolução final do trabalho. Falta casos testes e fazer a formatação do código para entrega final

--- src/pbgm/PBGM.java
+++ src/pbgm/PBGM.java
@@ -1,8 +1,6 @@
-
 package pbgm;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -10,128 +8,169 @@ import java.util.Scanner;
  * @author igor lemes
  */
 public class PBGM {
-private int numeroVertices = 5; 
-private int custoFinal = 0;
-private int maiorValor = 0;
+    
+    //Number of vertice received in the first line
+    private int totalVertice;
+    //Sum of all edges in the spanning tree
+    private int finalCost = 0;
+    //Biggest value found until now 
+    private int biggerValue = 0;
+    private int graph[][];
 
-
-public PBGM ( int v){
-    this.numeroVertices = v;
-}
-  
-    // A utility function to find the vertex with minimum key 
-    // value, from the set of vertices not yet included in MST 
-    int minKey(int key[], Boolean mstSet[]) 
-    { 
-        // Initialize min value 
-        int min = Integer.MAX_VALUE, min_index = -1; 
-  
-        for (int v = 0; v < numeroVertices; v++) 
-            if (mstSet[v] == false && key[v] < min) { 
-                min = key[v]; 
-                min_index = v; 
-            } 
-  
-        return min_index; 
-    } 
-  
-    // A utility function to print the constructed MST stored in 
-    // parent[] 
-    void printMST(int parent[], int graph[][]) {
-        System.out.println("Edge \tWeight");
-        for (int i = 1; i < numeroVertices; i++) {
-
-            this.custoFinal += graph[i][parent[i]];
-            if (this.maiorValor < graph[i][parent[i]]) {
-                this.maiorValor = graph[i][parent[i]];
+    public PBGM(int v) {
+        this.totalVertice = v;
+    }
+    
+    /*
+    Constructor of adjacency matrix of the graph 
+    */
+    public void createGraph() {
+        this.graph = new int[this.totalVertice][this.totalVertice];
+    }
+    
+    /*
+    Convert the inpunt from String to int and put it in the correct line o the graph
+    */
+    
+    public void setGraphLine(String line[], int lineIndex){
+        for(int i = 0; i < this.totalVertice;i ++){
+            this.graph[lineIndex][i] = Integer.parseInt(line[i]);
+        }
+    }
+    
+    /*
+    Just a function to print the graph
+    */
+    public void printGraph(){
+        for(int i= 0; i < this.totalVertice ; i++){
+            for(int j = 0; j < this.totalVertice ; j++){
+                System.out.print(this.graph[i][j] + " ");
             }
-            
+            System.out.println(" ");
+        }
+    }
+    
+    
+    /*
+    Function to see the vertice with minimum cost not included yet in the final solution
+    */
+    int minKey(int key[], Boolean mstSet[]) {
+
+        int min = Integer.MAX_VALUE, min_index = -1;
+
+        for (int v = 0; v < totalVertice; v++) {
+            if (mstSet[v] == false && key[v] < min) {
+                min = key[v];
+                min_index = v;
+            }
+        }
+
+        return min_index;
+    }
+
+    /*
+    Function to generate ONLY the total cost of the bi-forest as output of the program
+    To do that, the for loop go trough the MST generetaed by prim's algorithm and "cut" the highst value of a 
+    edge, creating then a forest with EXACTLY two conected components
+    */
+    void printResult(int parent[], int graph[][]) {
+        System.out.println("Edge \tWeight");
+        for (int i = 1; i < totalVertice; i++) {
+
+            this.finalCost += graph[i][parent[i]];
+            if (this.biggerValue < graph[i][parent[i]]) {
+                this.biggerValue = graph[i][parent[i]];
+            }
+
             System.out.println(parent[i] + " - " + i + "\t" + graph[i][parent[i]]);
         }
-        System.out.println("Maior valor encontrado é: " + this.maiorValor + " -- " + "Custo total: " + this.custoFinal);
+        
+        int custoMinimo = this.finalCost - this.biggerValue;
+        System.out.println("Maior valor encontrado é: " + this.biggerValue + " -- " + "Custo total: " + this.finalCost +
+                "  Custo minimo total = " +custoMinimo);
     }
-  
-    // Function to construct and print MST for a graph represented 
-    // using adjacency matrix representation 
-    void primMST(int graph[][]) 
-    { 
+
+    
+    /*
+    Function that implements the PRIM's Minimum Spanning tree algorithm and print the total cost of Prims
+    soluction used in a bi-forest
+    */
+    void primMST() {
         // Array to store constructed MST 
-        int parent[] = new int[numeroVertices]; 
-  
+        int parent[] = new int[totalVertice];
+
         // Key values used to pick minimum weight edge in cut 
-        int key[] = new int[numeroVertices]; 
-  
+        int key[] = new int[totalVertice];
+
         // To represent set of vertices not yet included in MST 
-        Boolean mstSet[] = new Boolean[numeroVertices]; 
-  
+        Boolean mstSet[] = new Boolean[totalVertice];
+
         // Initialize all keys as INFINITE 
-        for (int i = 0; i < numeroVertices; i++) { 
-            key[i] = Integer.MAX_VALUE; 
-            mstSet[i] = false; 
-        } 
-  
+        for (int i = 0; i < totalVertice; i++) {
+            key[i] = Integer.MAX_VALUE;
+            mstSet[i] = false;
+        }
+
         // Always include first 1st vertex in MST. 
-        key[0] = 0; // Make key 0 so that this vertex is 
-        // picked as first vertex 
-        parent[0] = -1; // First node is always root of MST 
-  
-        // The MST will have V vertices 
-        for (int count = 0; count < numeroVertices - 1; count++) { 
+        key[0] = 0; 
+        
+        parent[0] = -1; 
+
+
+        for (int count = 0; count < totalVertice - 1; count++) {
             // Pick thd minimum key vertex from the set of vertices 
             // not yet included in MST 
-            int u = minKey(key, mstSet); 
-  
+            int u = minKey(key, mstSet);
+
             // Add the picked vertex to the MST Set 
-            mstSet[u] = true; 
-  
+            mstSet[u] = true;
+
             // Update key value and parent index of the adjacent 
             // vertices of the picked vertex. Consider only those 
             // vertices which are not yet included in MST 
-            for (int v = 0; v < numeroVertices; v++) 
-  
-                // graph[u][v] is non zero only for adjacent vertices of m 
-                // mstSet[v] is false for vertices not yet included in MST 
-                // Update the key only if graph[u][v] is smaller than key[v] 
-                if (graph[u][v] != 0 && mstSet[v] == false && graph[u][v] < key[v]) { 
-                    parent[v] = u; 
-                    key[v] = graph[u][v]; 
-                } 
-        } 
-  
-        // print the constructed MST 
-        printMST(parent, graph); 
-    } 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
-        PBGM t = new PBGM(5); 
-        int graph[][] = new int[][] { { 0, 2, 0, 6, 0 }, 
-                                      { 2, 0, 3, 8, 5 }, 
-                                      { 0, 3, 0, 0, 7 }, 
-                                      { 6, 8, 0, 0, 9 }, 
-                                      { 0, 5, 7, 9, 0 } }; 
-        
-        System.out.println(graph.length);
-        
-        int numVert;
+            for (int v = 0; v < totalVertice; v++) 
+                
+            // Update the key only if graph[u][v] is smaller than key[v] 
+            {
+                if (this.graph[u][v] != -1 && mstSet[v] == false && this.graph[u][v] < key[v]) {
+                    parent[v] = u;
+                    key[v] = this.graph[u][v];
+                }
+            }
+        }
 
-        Scanner ler = new Scanner(System.in); // pega o número de vértices
-        numVert = Integer.parseInt(ler.nextLine());
-            
-        
-        
-        
-        
-		
-        for (int i = 0; i < numVert; i++) {
-			         System.out.println(ler.nextLine());
-		}
-
-		ler.close();    
-  
-        // Print the solution 
-        t.primMST(graph); 
+ 
+        printResult(parent, graph);
     }
+
+
+
     
+    public static void main(String[] args) throws IOException {
+       
+        Scanner ler = new Scanner(System.in); // Take the first line containing the total of vertices
+        
+        int numVert = Integer.parseInt(ler.nextLine()); 
+        
+        
+        PBGM pbgm = new PBGM (numVert); // Initialize the object with the total number of vertices
+        pbgm.createGraph();
+        
+        
+        /*
+        Loop to convert every line of the input to a Integer Array (passed to the adjacency matrix)
+        */
+        for (int i = 0; i < numVert; i++) {
+            String string = ler.nextLine();
+            String dividida[] = string.split(" ");
+            pbgm.setGraphLine(dividida, i);
+        
+        }
+        pbgm.printGraph();
+        ler.close();
+
+        // Execute the prim's algorithm and print the result
+        pbgm.primMST();
+    }
+
 }
